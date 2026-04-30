219e06ffbdc0b807bcc156633453dbf6b77d886a
Safwan Rahman <safwanmrahman@gmail.com>
2026-04-30 10:00:52 -0700

    Remove next.js build

--- .next/dev/server/chunks/ssr/_next-internal_server_app_club_[clubId]_page_actions_0.76ebi.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/club/[clubId]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_club_%5BclubId%5D_page_actions_0.76ebi.js.map
